8d4bb9b4e105fb39831d42d61ba57ce40748c7ac
Richard Walroth <rwalroth89@gmail.com>
2019-08-23 16:59:37 -0700

    Wrote the code for the process_data.py script

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,56 @@
 import sys
-
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+    
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+    
+    # merge datasets
+    df = messages.merge(categories, on=['id'])
+    
+    return df
 
 
 def clean_data(df):
-    pass
+    # load categories dataset
+    categories = df['categories'].str.split(';',expand=True)
+    
+    # select the first row of the categories dataframe
+    row = categories.loc[0]
+    
+    # use this row to extract a list of new column names for categories.
+    category_colnames = [x[:-2] for x in row]
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = [x[-1] for x in categories[column]]
+    
+    # convert column from string to numeric
+    categories[column] = pd.to_numeric(categories[column])
+    
+    # drop the original categories column from `df`
+    df = df.drop('categories', axis=1)
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat((df, categories), axis=1)
+    
+    # drop duplicates
+    df = df.drop_duplicates()
+    
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine(database_filename)
+    df.to_sql('InsertTableName', engine, index=False)
 
 
 def main():
